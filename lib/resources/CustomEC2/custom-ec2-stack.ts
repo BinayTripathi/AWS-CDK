e1d2d7acf3c17813c50d3225cf1a26be1b077b14
BinayTripathi <binay.mvkv@gmail.com>
2022-09-09 16:40:37 +1000

    Adding My EC2 stack

--- lib/resources/CustomEC2/custom-ec2-stack.ts
+++ lib/resources/CustomEC2/custom-ec2-stack.ts
@@ -1,6 +1,10 @@
 import { CfnOutput, Stack, StackProps, Tags } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_ec2 as _ec2 } from 'aws-cdk-lib';
+import { aws_iam as _iam } from 'aws-cdk-lib';
+
+import { readFileSync, promises as fsPromises } from 'fs';
+import { join } from 'path';
 
 export class CustomEC2Stack extends Stack {
 
@@ -26,9 +30,38 @@ export class CustomEC2Stack extends Stack {
         machineImage: new _ec2.AmazonLinuxImage({
             generation: _ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
             kernel: _ec2.AmazonLinuxKernel.KERNEL5_X,
-          })
+            storage: _ec2.AmazonLinuxStorage.EBS
+          }),
+          userData: _ec2.UserData.custom(syncReadFile("install_httpd.sh")),
     })
 
+    //Adding connection - through security group or a defaut allow type
+    web_server.connections.allowFromAnyIpv4(_ec2.Port.tcp(80), "Allowing all web traffic on TCP")
+
+    //Add permissions to webserver instance profile to login without ssh or push logs
+    web_server.role.addManagedPolicy(
+      _iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
+    )
+
+    //Add permissions to webserver to access S3
+    web_server.role.addManagedPolicy(
+      _iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonS3ReadOnlyAccess")
+    )
+
+    //Exporting Ip address as output
+    var output1 = new CfnOutput(this, "WebServerIp", {
+        description: 'Webservers public ip address',
+        value: `http://${web_server.instancePublicIp}`,
+    })
+
+    
+
+
+
+    function syncReadFile(filename: string) {
+        const result = readFileSync(join(__dirname,'/bootstrap_scripts', filename), 'utf-8');
+        return result;
+      }
      
-}
+ }
 }           
